4eea76e9b434a94ea6f3340f28fcc79d75e8f63a
nico.padilla <nico_padi@hotmail.com>
2026-07-26 19:09:15 -0300

    v0.2 - Base del ERP con FastAPI y SQLite

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="ERP María Paz Showroom",
     version="0.1.0"
 )
 
+
 @app.get("/")
 def inicio():
     return {
         "mensaje": "Bienvenido al ERP María Paz Showroom",
-        "estado": "Funcionando"
+        "estado": "Sistema funcionando",
+        "base_datos": "Conectada"
     }
