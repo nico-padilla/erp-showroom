195f79b15989328fc78db7f082a9bb20e5b21192
Nicolas Padilla <nico_padi@hotmail.com>
2026-08-15 18:56:16 -0300

    Conectar componentes con API de Render

--- frontend/src/components/Clientes.jsx
+++ frontend/src/components/Clientes.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const API = "http://127.0.0.1:8000"
+const API = import.meta.env.VITE_API_URL
 
 export default function Clientes() {
   const [clientes, setClientes] = useState([])
